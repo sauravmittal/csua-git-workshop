b9ca36a2d65156c99df587bbf357d43319f983b5
Robert <robertquitt@gmail.com>
2017-09-08 04:17:13 -0700

    Add best.py

---
+++ best.py
@@ -0,0 +1,8 @@
+def main():
+    name = "Hilfinger"
+    superlative = "best"
+    position = "professor"
+    print("{0} is the {1} {2}!".format(name, superlative, position))
+
+if __name__ == '__main__':
+    main()
