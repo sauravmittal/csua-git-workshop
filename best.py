b3bbfeb34bc876742298cd787c8837542424508e
aaronhoby <aaronhoby@gmail.com>
2017-09-08 18:21:49 -0700

    I made changes!!! #

--- best.py
+++ best.py
@@ -1,12 +1,12 @@
 
 def main():
-    person_a = "Person A"
-    superlative_a = "best"
+    person_a = "Aaron Ngai"
+    superlative_a = "[insert amazing adjective]"
 
     person_b = "Saurav Mittal"
     superlative_b = "*something clever*"
 
-    name = "Hilfinger"
+    name = "Daddy Dirks"
     position = "professor"
     print("{0}: {1} is the {2} {3}!".format(person_a, name, superlative_a, position))
     print("{0}: {1} is the {2} {3}!".format(person_b, name, superlative_b, position))
