f6e4c8254f519445aa600730405285ca53a30df8
Robert Quitt <robertquitt@berkeley.edu>
2017-09-08 17:09:26 -0700

    Modify best.py

--- best.py
+++ best.py
@@ -1,8 +1,15 @@
+
 def main():
+    person_a = "Person A"
+    superlative_a = "best"
+
+    person_b = "Person B"
+    superlative_b = "worst"
+
     name = "Hilfinger"
-    superlative = "best"
     position = "professor"
-    print("{0} is the {1} {2}!".format(name, superlative, position))
+    print("{0}: {1} is the {2} {3}!".format(person_a, name, superlative_a, position))
+    print("{0}: {1} is the {2} {3}!".format(person_b, name, superlative_b, position))
 
 if __name__ == '__main__':
     main()
