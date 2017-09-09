779a2738b6e8fbf4cd8a61543dca6b96ec4d3d88
sauravmittal <sauravmitt3@gmail.com>
2017-09-08 18:27:22 -0700

    new daddy

--- best.py
+++ best.py
@@ -6,7 +6,7 @@ def main():
     person_b = "Saurav Mittal"
     superlative_b = "*something clever*"
 
-    name = "Daddy Dirks"
+    name = "Daddy DeNero"
     position = "professor"
     print("{0}: {1} is the {2} {3}!".format(person_a, name, superlative_a, position))
     print("{0}: {1} is the {2} {3}!".format(person_b, name, superlative_b, position))
