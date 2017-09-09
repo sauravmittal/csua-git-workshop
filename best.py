c3be9daaccdba0d08029fdcc1f6888eb3dff8fec
sauravmittal <sauravmitt3@gmail.com>
2017-09-08 18:38:17 -0700

    random changes

--- best.py
+++ best.py
@@ -4,10 +4,10 @@ def main():
     superlative_a = "[insert amazing adjective]"
 
     person_b = "Saurav Mittal"
-    superlative_b = "*something clever*"
+    superlative_b = "[insert something clever]"
 
-    name = "Daddy DeNero"
-    position = "professor"
+    name = "eecseecseecseecs"
+    position = "4.00000"
     print("{0}: {1} is the {2} {3}!".format(person_a, name, superlative_a, position))
     print("{0}: {1} is the {2} {3}!".format(person_b, name, superlative_b, position))
 
